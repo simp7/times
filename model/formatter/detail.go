0141de3747c094152863c227f4a2ff30d98d8e18
simp7 <JHParkOfficial@protonmail.com>
2021-02-04 10:54:34 +0900

    Fix some bugs in formatter and introduce clock formatter

--- model/formatter/detail.go
+++ model/formatter/detail.go
@@ -1,7 +1,6 @@
 package formatter
 
 import (
-	"fmt"
 	"github.com/simp7/times/model/tobject"
 )
 
@@ -12,18 +11,11 @@ type detailFormatter struct {
 //Detail returns one of struct that implements TimeFormatter.
 //Detail shows time like 0:00:000, And It can express time unit from millisecond to day.
 func Detail() TimeFormatter {
-	t := new(detailFormatter)
-	return t
+	return new(detailFormatter)
 }
 
 func (f *detailFormatter) Format(t tobject.Time) string {
 	result := f.standardFormatter.Format(t)
-	if t.MilliSecond() < 10 {
-		result += fmt.Sprintf(":00%d", t.MilliSecond())
-	} else if t.MilliSecond() < 100 {
-		result += fmt.Sprintf(":0%d", t.MilliSecond())
-	} else {
-		result += fmt.Sprintf(":%d", t.MilliSecond())
-	}
+	result = result + ":" + tripleDigitFormat(t.MilliSecond())
 	return result
 }
--- model/formatter/timeFormatter.go
+++ model/formatter/timeFormatter.go
@@ -1,6 +1,7 @@
 package formatter
 
 import (
+	"fmt"
 	"github.com/simp7/times/model/tobject"
 )
 
@@ -8,4 +9,20 @@ import (
 //TimeFormatter is used in gadgets for the use of showing times.
 type TimeFormatter interface {
 	Format(t tobject.Time) string //Format converts tobject.Time into string. This function can be used in structs that implement gadget.Gadget
+}
+
+func doubleDigitFormat(a int) string {
+	if a < 10 {
+		return fmt.Sprintf("0%d", a)
+	}
+	return fmt.Sprintf("%d", a)
+}
+
+func tripleDigitFormat(a int) string {
+	if a < 10 {
+		return fmt.Sprintf("00%d", a)
+	} else if a < 100 {
+		return fmt.Sprintf("0%d", a)
+	}
+	return fmt.Sprintf("%d", a)
 }
